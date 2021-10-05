44c9ecbc928b737306527824f57ffb39cb84d1f4
Peter Kroeger <39171216+KroegerP@users.noreply.github.com>
2021-10-05 16:28:06 -0400

    Basic setup

---
+++ Machine Learning HW 3.py
@@ -0,0 +1,9 @@
+# Machine Learning HW 3
+# Yayha Emara, Peter Kroeger, Ryan Rubadue, Ryan Kunkel, Griffin Ramsey
+
+import csv
+# from sklearn.datasets import load_iris
+
+open('iris.csv', newline='')
+iris = load_iris()
+iris
